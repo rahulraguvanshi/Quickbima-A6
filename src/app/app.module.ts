44f544a2c36c0b870176fd7354722a09b46e8ef1
Rahul Raghuvanshi <rahulraguvanshi@gmail.com>
2018-06-07 09:09:39 +0530

    1st update

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { AppLayoutComponent } from './_layout/app-layout/app-layout.component';
+import { SiteLayoutComponent } from './_layout/site-layout/site-layout.component';
+import { HomeLayoutComponent } from './_layout/home-layout/home-layout.component';
+import { HomeHeaderComponent } from './_layout/home-header/home-header.component';
+import { AppHeaderComponent } from './_layout/app-header/app-header.component';
+import { SiteHeaderComponent } from './_layout/site-header/site-header.component';
+import { SiteFooterComponent } from './_layout/site-footer/site-footer.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AboutComponent } from './about/about.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+
+import { routing } from './app.routing';
+import { CarFormComponent } from './car-form/car-form.component';
+import { HealthFormComponent } from './health-form/health-form.component';
+import { HomeContentComponent } from './home-content/home-content.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, routing ],
+  declarations: [ 
+    AppComponent, 
+    HelloComponent, 
+    AppLayoutComponent, 
+    SiteLayoutComponent,
+    HomeLayoutComponent,
+    AppHeaderComponent, 
+    SiteHeaderComponent, 
+    SiteFooterComponent, 
+    LoginComponent, 
+    DashboardComponent,
+    AboutComponent, 
+    RegisterComponent, 
+    ProfileComponent,
+    HomeHeaderComponent,
+    CarFormComponent,
+    HealthFormComponent,
+    HomeContentComponent
+  ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,55 @@
+import { Routes, RouterModule } from '@angular/router';
+
+
+import { SiteLayoutComponent } from './_layout/site-layout/site-layout.component';
+import { AppLayoutComponent } from './_layout/app-layout/app-layout.component';
+import { HomeLayoutComponent } from './_layout/home-layout/home-layout.component';
+
+import { HomeContentComponent } from './home-content/home-content.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
+import { CarFormComponent } from './car-form/car-form.component';
+import { HealthFormComponent } from './health-form/health-form.component';
+
+const appRoutes: Routes = [
+    //Homepage routes goes here
+    { 
+        path: '', 
+        component: HomeLayoutComponent,
+        children: [
+          { path: '', component: CarFormComponent },
+          { path: 'healthinsurance', component: HealthFormComponent }
+        ]
+    },
+    //Site routes goes here
+    { 
+        path: '', 
+        component: SiteLayoutComponent,
+        children: [
+          { path: 'about', component: AboutComponent },
+          { path: 'test/:id', component: AboutComponent }
+        ]
+    },
+    // App routes goes here here
+    { 
+        path: '',
+        component: AppLayoutComponent, 
+        children: [
+          { path: 'dashboard', component: DashboardComponent },
+          { path: 'profile', component: ProfileComponent }
+        ]
+    },
+
+    //no layout routes
+    { path: 'login', component: LoginComponent},
+    { path: 'register', component: RegisterComponent },
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
+
+
